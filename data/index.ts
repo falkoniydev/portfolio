d0edfe96689fb5cecca977f273c4feab613c1c91
falkoniydev <falkoniydev@gmail.com>
2024-06-28 01:23:51 +0500

    Portfolio

---
+++ data/index.ts
@@ -0,0 +1,352 @@
+export const navItems = [
+	{ name: "About", link: "#about" },
+	{ name: "Projects", link: "#projects" },
+	{ name: "Testimonials", link: "#testimonials" },
+	{ name: "Contact", link: "#contact" },
+];
+
+// export const gridItems = [
+//   {
+//     id: 1,
+//     title: "I prioritize client collaboration, fostering open communication ",
+//     description: "",
+//     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+//     imgClassName: "w-full h-full",
+//     titleClassName: "justify-end",
+//     img: "/bg1.jpg",
+//     spareImg: "",
+//   },
+//   {
+//     id: 2,
+//     title: "I'm very flexible with time zone communications",
+//     description: "",
+//     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+//     imgClassName: "",
+//     titleClassName: "justify-start",
+//     img: "",
+//     spareImg: "",
+//   },
+//   {
+//     id: 3,
+//     title: "My tech stack",
+//     description: "I constantly try to improve",
+//     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+//     imgClassName: "",
+//     titleClassName: "justify-center",
+//     img: "",
+//     spareImg: "",
+//   },
+//   {
+//     id: 4,
+//     title: "Tech enthusiast with a passion for development.",
+//     description: "",
+//     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+//     imgClassName: "",
+//     titleClassName: "justify-start",
+//     img: "/grid.svg",
+//     spareImg: "/b4.svg",
+//   },
+
+//   {
+//     id: 5,
+//     title: "Currently building a JS Animation library",
+//     description: "The Inside Scoop",
+//     className: "md:col-span-3 md:row-span-2",
+//     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+//     titleClassName: "justify-center md:justify-start lg:justify-center",
+//     img: "/b5.svg",
+//     spareImg: "/grid.svg",
+//   },
+//   {
+//     id: 6,
+//     title: "Do you want to start a project together?",
+//     description: "",
+//     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+//     imgClassName: "",
+//     titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+//     img: "",
+//     spareImg: "",
+//   },
+// ];
+
+// export const projects = [
+//   {
+//     id: 1,
+//     title: "3D Solar System Planets to Explore",
+//     des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+//     img: "/p1.svg",
+//     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+//     link: "/ui.earth.com",
+//   },
+//   {
+//     id: 2,
+//     title: "Yoom - Video Conferencing App",
+//     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+//     img: "/p2.svg",
+//     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+//     link: "/ui.yoom.com",
+//   },
+//   {
+//     id: 3,
+//     title: "AI Image SaaS - Canva Application",
+//     des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+//     img: "/p3.svg",
+//     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+//     link: "/ui.aiimg.com",
+//   },
+//   {
+//     id: 4,
+//     title: "Animated Apple Iphone 3D Website",
+//     des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+//     img: "/p4.svg",
+//     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+//     link: "/ui.apple.com",
+//   },
+// ];
+
+export const gridItems = [
+	{
+		id: 1,
+		title: "I prioritize client collaboration, fostering open communication ",
+		description: "",
+		className:
+			"lg:col-span-3 md:col-span-6 md:row-span-4 max-sm:w-[100%] max-sm:h-[100%] lg:min-h-[60vh]",
+		imgClassName: "w-full h-full",
+		titleClassName: "justify-end",
+		img: "/bg1.jpg",
+		spareImg: "",
+	},
+	{
+		id: 2,
+		title: "I'm very flexible with time zone communications",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+		imgClassName: "",
+		titleClassName: "justify-start",
+		img: "",
+		spareImg: "",
+	},
+	{
+		id: 3,
+		title: "My tech stack",
+		description: "I constantly try to improve",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+		imgClassName: "",
+		titleClassName: "justify-center",
+		img: "",
+		spareImg: "",
+	},
+	{
+		id: 4,
+		title: "Tech enthusiast with a passion for development.",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+		imgClassName: "",
+		titleClassName: "justify-start",
+		img: "/grid.svg",
+		spareImg: "/b4.svg",
+	},
+
+	{
+		id: 5,
+		title: "Currently building a JS Animation library",
+		description: "The Inside Scoop",
+		className: "md:col-span-3 md:row-span-2",
+		imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+		titleClassName: "justify-center md:justify-start lg:justify-center",
+		img: "/b5.svg",
+		spareImg: "/grid.svg",
+	},
+	{
+		id: 6,
+		title: "Do you want to start a project together?",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+		imgClassName: "",
+		titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+		img: "",
+		spareImg: "",
+	},
+];
+
+export const projects = [
+	{
+		id: 1,
+		title: "Cryptofolio Project",
+		des: "An open-source software program that allows users to track their cryptocurrency holdings. It offers a web interface, mobile app, and desktop application, giving users flexibility in how they manage their crypto portfolio.",
+		img: "/cryptofolio-pic.png",
+		iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+		link: "https://cryptofolio-nu.vercel.app/",
+		name: "Cryptofolio",
+		SourceCode: "https://github.com/mrbillgates2001/Final_Exam_Najot",
+	},
+	{
+		id: 2,
+		title: "Weather Forecast Application",
+		des: "Weather App is your comprehensive and user-friendly weather app, providing you with up-to-date and accurate weather information wherever you go.  With beautiful and intuitive design,  [App Name] makes it easy to plan your day with confidence.",
+		img: "/weather-app-pic.png",
+		iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+		link: "https://project-2-open-weather.vercel.app/",
+		name: "WeatherApp",
+		SourceCode: "https://github.com/falkoniydev/project-2-open-weather",
+	},
+	{
+		id: 3,
+		title: "Recipe Finder For Best Culinary",
+		des: "Recipe Finder is your ultimate companion for whipping up delicious and creative meals. No more staring blankly at your pantry, wondering what to make. Recipe Finder helps you transform your existing ingredients into culinary masterpieces.",
+		img: "/recipe-Finder-pic.png",
+		iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+		link: "https://project-3-recipe-finder.vercel.app/",
+		name: "RecipeFinder",
+		SourceCode: "https://github.com/falkoniydev/project-3-recipe-finder",
+	},
+	{
+		id: 4,
+		title: "Your Todo List is Here",
+		des: "Capture Everything: Brainstorm freely! Jot down tasks, ideas, and reminders with ease using our intuitive interface.",
+		img: "/todo-app-pic.png",
+		iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+		link: "https://project-1-todo-app.vercel.app/",
+		name: "TodoList",
+		SourceCode: "https://github.com/falkoniydev/project-1-todo-app",
+	},
+];
+
+export const testimonials = [
+	{
+		quote:
+			"Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote:
+			"Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote:
+			"Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote:
+			"Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote:
+			"Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+];
+
+export const companies = [
+	{
+		id: 1,
+		name: "JavaScript",
+		img: "/javascript.svg",
+		nameImg: "/appName.svg",
+	},
+	{
+		id: 2,
+		name: "TypeScript",
+		img: "/ts.svg",
+		nameImg: "/hostName.svg",
+	},
+	{
+		id: 3,
+		name: "React JS",
+		img: "re.svg",
+		nameImg: "/cloudName.svg",
+	},
+	{
+		id: 4,
+		name: "Next JS",
+		img: "/next.svg",
+		nameImg: "/streamName.svg",
+	},
+	{
+		id: 5,
+		name: "Tailwind CSS",
+		img: "/tail.svg",
+		nameImg: "/dockerName.svg",
+	},
+	{
+		id: 6,
+		name: "Material UI",
+		img: "/mui.svg",
+		nameImg: "/dockerName.svg",
+	},
+	{
+		id: 7,
+		name: "HTML",
+		img: "/html.svg",
+		nameImg: "/dockerName.svg",
+	},
+	{
+		id: 8,
+		name: "CSS",
+		img: "/css.svg",
+		nameImg: "/dockerName.svg",
+	},
+	{
+		id: 9,
+		name: "GitHub",
+		img: "/git.svg",
+		nameImg: "/dockerName.svg",
+	},
+];
+
+export const workExperience = [
+	{
+		id: 1,
+		title: "Frontend Engineer Intern",
+		desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+		className: "md:col-span-2",
+		thumbnail: "/exp1.svg",
+	},
+	{
+		id: 2,
+		title: "Mobile App Dev - JSM Tech",
+		desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+		className: "md:col-span-2", // change to md:col-span-2
+		thumbnail: "/exp2.svg",
+	},
+	{
+		id: 3,
+		title: "Freelance App Dev Project",
+		desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+		className: "md:col-span-2", // change to md:col-span-2
+		thumbnail: "/exp3.svg",
+	},
+	{
+		id: 4,
+		title: "Lead Frontend Developer",
+		desc: "Developed and maintained user-facing features using modern frontend technologies.",
+		className: "md:col-span-2",
+		thumbnail: "/exp4.svg",
+	},
+];
+
+export const socialMedia = [
+	{
+		id: 1,
+		img: "/git.svg",
+		href: "https://github.com/falkoniydev",
+	},
+	{
+		id: 2,
+		img: "/insta.svg",
+		href: "https://www.instagram.com/falkoniy",
+	},
+	{
+		id: 3,
+		img: "/link.svg",
+		href: "https://www.linkedin.com/in/shokhboz-nabiev",
+	},
+];
